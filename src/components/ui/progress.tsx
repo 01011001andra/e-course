644cd5a7a3d10b804aad985afa5f0d3dc8b753b0
Yandra Muslim <yandrayt1@gmail.com>
2024-08-26 19:15:06 +0700

    fix: error build at vercel

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,10 +5,15 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  isCompleted?: boolean;
+}
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ isCompleted, className, value, ...props }, ref) => (
+  ProgressProps
+>(({ className, value, isCompleted, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
